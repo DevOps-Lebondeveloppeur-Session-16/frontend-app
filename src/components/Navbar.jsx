894c46ac50f94a33f7990f66b56ef31e52290b24
Talel Ben Belgacem <talelbenbelgacem12@gmail.com>
2026-02-27 22:59:21 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 					bgGradient={"linear(to-r, cyan.400, blue.500)"}
 					bgClip={"text"}
 				>
-					<Link to={"/"}>🚀 Le bon Developpeur - Store 🛒</Link>
+					<Link to={"/"}>🚀🚀🚀🚀 Le bon Developpeur - Store 🛒</Link>
 				</Text>
 
 				<HStack spacing={2} alignItems={"center"}>
